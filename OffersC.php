f4d1e58e90c3df018bc32d5a3df68909653cbc42
lyn815 <1248lyn@gmail.com>
2013-04-27 10:50:20 -0300

    Create OffersC.php

---
+++ OffersC.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--http://lyn815.github.io/Cloudcw/OffersC.php-->
+<html>
+
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+
+  <body>
+    <h1>Restaurant offers</h1>
+    <?php
+    $restname=$_GET['rest'];
+    echo "<em>$restname<em>";
+    ?>
+    
+    <h2>Offer 1</h2>
+    
+  </body>
+
+</html>
